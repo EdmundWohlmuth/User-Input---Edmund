d782899f07c9b7dac19e3a22e80e05a5860bdf9e
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-13 15:29:44 -0400

    Finalised work

--- Assets/Scripts/GetAxis.cs
+++ Assets/Scripts/GetAxis.cs
@@ -16,10 +16,18 @@ public class GetAxis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // getaxis looks for float (-1.0 - 1.0)
         verticalSpeed = Input.GetAxis("Vertical") * Time.deltaTime;
         transform.Translate(0, 0, verticalSpeed);
 
         horizontalSpeed = Input.GetAxis("Horizontal") * Time.deltaTime;
         transform.Translate(horizontalSpeed, 0, 0);
+
+        // alternativy...
+        // Vector3 movement
+        // movement.x
+        // movement.y
+        // movement.z
+        // above defines the Vector3 "Movement"
     }
 }
